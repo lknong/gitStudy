d181046905d365cce20364f8ec1fe92ac48a6107
liangkeng <liangkengnong@168.com>
2021-05-05 17:51:03 +0800

    second commit hot-fix

---
+++ src/test/java/com/atguigu/gitstudy/Test.java
@@ -0,0 +1,10 @@
+package com.atguigu.gitstudy;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123123");
+        System.out.println("123123");
+        System.out.println("123123");
+        System.out.println("123123");
+    }
+}
