40e00f63f28e62f664bd244b9a994c216b54f7c2
liangkeng <liangkengnong@168.com>
2021-05-06 22:35:03 +0800

    pust test commit

--- src/test/java/com/atguigu/gitstudy/Test.java
+++ src/test/java/com/atguigu/gitstudy/Test.java
@@ -11,5 +11,6 @@ public class Test {
         System.out.println("123123");
         System.out.println("hot-fix");
         System.out.println("hot-fix");
+        System.out.println("push-test");
     }
 }
